a48c2a6bf90bf55a4353ad6d5359366145a95052
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-10-06 01:10:57 +0300

    routing

--- src/Aboutus/About.js
+++ src/Aboutus/About.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "/home/moringa/Desktop/React-project/upexpress/src/Aboutus/About.css";
+import EngImg from "../images/mechanics.jpeg";
 
-function AboutUs() {
-  return <>About US Page</>;
+export default function AboutUs() {
+  return (
+    <>
+      <h1 className="Mesg">We Are Commited To Serving You </h1>
+      <div className="Majormsg">
+        <p>That is why we Ensure we give you nothing but the BEST</p>
+      </div>
+
+      <div className="Mechanics">
+        <p className="Mmsg">
+          <h2>Meet Our Engineering Team</h2>
+          These are people behind physical or mechanical systems, such as power
+          and energy systems<p></p>these people ensure the safety of our fleet
+        </p>
+        <img className="mechanicsImg" src={EngImg} alt="mechanics" />
+      </div>
+    </>
+  );
 }
-
-export default AboutUs;
