97c8c1439b75aaf427e643b1512fca5337743595
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-10-07 01:48:25 +0300

    final commit

--- src/BookingPage/Booking.js
+++ src/BookingPage/Booking.js
@@ -1,22 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../BookingPage/booking.css";
 // import Records from "../Records.json";
 
+const baseUrl = "http://localhost:8002/routes/";
+
 export default function Booking() {
+  const [busRoutes, setBusRoutes] = useState([]);
+  const [selectedRoute, setSelectedRoute] = useState("");
+  const [departureTime, setDepartureTime] = useState("");
+
+  useEffect(function useEffectFunction() {
+    fetch(baseUrl)
+      .then((response) => response.json())
+      .then((result) => {
+        setBusRoutes(result);
+        console.log("routes" + result);
+      });
+  }, []);
+
+  function handleclick(e) {
+    e.preventDefault();
+    setSelectedRoute(document.getElementById("routes").value);
+    setDepartureTime(document.getElementById("date").value);
+  }
+
   return (
     <div className="formcontainer">
       <form className="form">
-        <label>Leaving from</label>
-        <input placeholder="leaving from" type="text"></input>
-        <label>Travelling to</label>
-        <input placeholder="leaving from" type="text"></input>
+        <label>Route</label>
+        <select name="routes" id="routes">
+          {busRoutes?.map((busRoute, i) => (
+            <option key={i} value={busRoute.route}>
+              {busRoute.route}
+            </option>
+          ))}
+        </select>
+        {/* <label>Travelling to</label> */}
+        {/* <input placeholder="leaving from" type="text"></input> */}
         <label>Travelling on</label>
-        <input placeholder="leaving from" type="date"></input>
+        <input id="date" type="date"></input>
         <label>Returning on</label>
         <input placeholder="leaving from" type="date"></input>
 
-        <button type="submit"> Book</button>
+        <button onClick={(e) => handleclick(e)} type="submit">
+          Book A Receipt
+        </button>
       </form>
+
+      <div className="receipt">
+        <h2>YOUR RECEIPT</h2>
+        <p>Route:{selectedRoute}</p>
+        <p>Departure Date:{departureTime}</p>
+      </div>
     </div>
   );
 }
