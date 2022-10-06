43b64b2f9b8199fdbb297eda9785fcba1656a404
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-10-06 20:07:05 +0300

    fetching  json

--- src/BookingPage/Booking.js
+++ src/BookingPage/Booking.js
@@ -2,11 +2,5 @@ import React from "react";
 import Records from "../Records.json";
 
 export default function Booking() {
-  return (
-    <div className="book">
-      {Records.map((record) => {
-        return <>{record.id}</>;
-      })}
-    </div>
-  );
+  return;
 }
