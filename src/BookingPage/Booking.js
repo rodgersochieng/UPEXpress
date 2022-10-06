9b525689c1e46e3d4b830acbed79eb6b5544fb81
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-10-06 22:38:32 +0300

    updating README

--- src/BookingPage/Booking.js
+++ src/BookingPage/Booking.js
@@ -1,12 +1,6 @@
 import React from "react";
-import Records from "../Records.json";
+// import Records from "../Records.json";
 
 export default function Booking() {
-  return (
-    <>
-      <form action="">
-        <input placeholder=""></input>
-      </form>
-    </>
-  );
+  return <></>;
 }
