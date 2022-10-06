93dc278d50d754f3856aba4ec46421efa7b07601
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-10-06 19:39:10 +0300

    fetching  json

--- src/BookingPage/Booking.js
+++ src/BookingPage/Booking.js
@@ -3,12 +3,6 @@ import Routes from "../db.json";
 
 export default function Booking() {
   return (
-    <>
-      <div className="Routes">
-        <h1>Available Routes</h1>
-        <br /> <br />
-        {}
-      </div>
-    </>
+    
   );
 }
