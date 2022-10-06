4b44d1332f2dd3522d37c138db7f966c807ff610
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-10-06 23:46:10 +0300

    final touches

--- src/BookingPage/Booking.js
+++ src/BookingPage/Booking.js
@@ -1,6 +1,22 @@
 import React from "react";
+import "../BookingPage/booking.css";
 // import Records from "../Records.json";
 
 export default function Booking() {
-  return <></>;
+  return (
+    <div className="formcontainer">
+      <form className="form">
+        <label>Leaving from</label>
+        <input placeholder="leaving from" type="text"></input>
+        <label>Travelling to</label>
+        <input placeholder="leaving from" type="text"></input>
+        <label>Travelling on</label>
+        <input placeholder="leaving from" type="date"></input>
+        <label>Returning on</label>
+        <input placeholder="leaving from" type="date"></input>
+
+        <button type="submit"> Book</button>
+      </form>
+    </div>
+  );
 }
